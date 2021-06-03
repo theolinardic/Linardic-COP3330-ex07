6e56125c7f56fc024aa1b70483d58f77325d4207
theol <theolinardic@gmail.com>
2021-06-03 15:12:06 -0400

    Initial commit

---
+++ src/main/java/oop/ex07/ex07.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Exercise 06 Solution
+ *  Copyright 2021 Theodore Linardic
+ */
+
+
+package oop.ex07;
+import java.util.Scanner;
+
+public class ex07 {
+    public static void main(String[] args)
+    {
+        double feetToMetersMultiplicationConstant = 0.09290304;
+
+        Scanner userInputScanner = new Scanner(System.in);
+        System.out.print("What is the length of the room in feet? ");
+        String roomLength = userInputScanner.nextLine();
+        System.out.print("What is the width of the room in feet? ");
+        String roomWidth = userInputScanner.nextLine();
+        System.out.println("You entered dimensions of " + roomLength + " feet by " + roomWidth + " feet.\nThe area is:");
+
+        int roomLengthInt = Integer.parseInt(roomLength);
+        int roomWidthInt = Integer.parseInt(roomWidth);
+        int totalSquareFeet = roomWidthInt * roomLengthInt;
+
+        double totalSquareMeters = totalSquareFeet * feetToMetersMultiplicationConstant;
+
+        System.out.print(totalSquareFeet + " square feet\n" + totalSquareMeters + " square meters");
+
+    }
+}
